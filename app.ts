c8bee23b0db85d7d0cb9a33863b0b04b5b50e422
lucas8790 <lv8790121@gmail.com>
2024-09-17 08:59:36 -0300

    Criando e usand um Autobind

--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
+// Autobind Decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get(){
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -22,6 +36,7 @@ class ProjectInput {
         this.attach();
     }
 
+    @autobind
     private submitHandler(event: Event){
         event.preventDefault();
         console.log(this.titleInputElement.value);
