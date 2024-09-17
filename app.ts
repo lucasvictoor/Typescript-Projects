a6fc87f69509435a0e17e6a2094337b599020232
lucas8790 <lv8790121@gmail.com>
2024-09-17 20:54:30 -0300

    Usando get

--- app.ts
+++ app.ts
@@ -123,6 +123,14 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor){
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     private project: Project;
 
+    get persons() {
+        if(this.project.people === 1){
+            return '1 person';
+        }else {
+            return `${this.project.people} persons`;
+        }
+    }
+
     constructor(hostId: string, project: Project) {
         super('single-project', hostId, false, project.id);
         this.project = project;
@@ -135,9 +143,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
     renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
         this.element.querySelector('p')!.textContent = this.project.description;
-
     }
 }
 
