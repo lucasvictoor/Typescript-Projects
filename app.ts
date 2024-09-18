b5185db18f6e90053a56918ce4ac3e04e0b8e2de
lucas8790 <lv8790121@gmail.com>
2024-09-18 09:38:45 -0300

    Finalizado o Drop & Drag

--- app.ts
+++ app.ts
@@ -55,6 +55,18 @@ class ProjectState extends State<Project>{
     addProject(title: string, description: string, num0fPeople: number){
         const newProject = new Project(Math.random().toString(), title, description, num0fPeople, ProjectStatus.Active);
         this.projects.push(newProject);
+        this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+         const project = this.projects.find(prj => prj.id === projectId);
+         if(project && project.status !== newStatus){
+            project.status = newStatus;
+            this.updateListeners();
+         }
+    }
+
+    private updateListeners(){
         for(const listenerFn of this.listeners){
             listenerFn(this.projects.slice());
         }
@@ -194,8 +206,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         }
     }
 
+    @autobind
     dropHandler(event: DragEvent){
-        console.log(event.dataTransfer!.getData('text/plain'));
+        const prjId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
     }
 
     @autobind
